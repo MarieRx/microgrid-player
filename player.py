01ee6feddef99dcfb6e505840a98b72968e46d1b
marie <marie.ruaux@laposte.net>
2021-05-11 17:54:40 +0200

    V1g fonctionne à peu près

--- player.py
+++ player.py
@@ -19,25 +19,46 @@ class Player:
 
 	def set_scenario(self, scenario_data):
 		self.data = scenario_data
-		arr_dep=list(scenario_data.values())[:self.nb_slow+self.nb_fast]
+		self.depart=list(scenario_data[(scenario_data["day"] == "01/01/2014")]["time_slot_dep"][:p.nb_slow + p.nb_fast])
+		self.arr = list(scenario_data[(scenario_data["day"] == "01/01/2014")]["time_slot_arr"][:p.nb_slow + p.nb_fast])
 
 	def set_prices(self, prices):
 		self.prices = prices
 
 	def compute_all_load(self):
 		load = np.zeros(self.horizon)
+		l=np.zeros(self.horizon)
 		chargement=np.zeros(self.nb_slow+self.nb_fast)
 		for time in range(self.horizon):
 			consom=0
 			for i in range(self.nb_slow):
-				chargement[i]+=self.rho_c*min(self.pslow,10-self.chargement[i],40-consom,0)
-				consom+=chargement[i]
+				plus = self.rho_c * min(self.pslow, min((10 - chargement[i])/self.rho_c, 40 - consom))
+				chargement[i] += plus
+				consom += plus
+
 
 			for i in range(self.nb_slow,self.nb_slow+self.nb_fast):
-				chargement[i]+=self.rho_c*min(self.pfast,10-self.chargement[i],40-consom,0)
-				consom+=chargement[i]
-			load[time]=consom
+				plus=self.rho_c*min(self.pfast,min((10-chargement[i])/self.rho_c,40-consom))
+				chargement[i]+=plus
+				consom+=plus
+
+			l[time]=consom
+		cpt=self.horizon-1
+		m = np.min(self.depart)
+		copie_prix=self.prices[:m].copy()
+		print(copie_prix)
+		arg_max=0
+		cpt=0
+		while cpt<m:
+			arg_min = np.argmin(copie_prix)
+			arg_max=np.argmax(l)
+			load[arg_min]=l[arg_max]
+			copie_prix[arg_min] = np.inf
+			l[arg_max] = 0
+			cpt+=1
 			#load[time] = self.compute_load(time)
+		print(copie_prix)
+		print(self.prices)
 		return load
 
 	def take_decision(self, time):
@@ -53,5 +74,18 @@ class Player:
 		# reset all observed data
 		pass
 
-file=pa.read_csv(ev_scenarios.csv)
-print(list(file[0]) )
+f=pa.read_csv("ev_scenarios.csv",";")
+p=Player()
+p.__init__()
+p.set_scenario(f)
+p.set_prices(random_lambda)
+print(p.prices)
+
+l=p.compute_all_load()
+print(l)
+def cout(p,l):
+	c=0
+	for time in range(48):
+		c+=l[time]*p[time]
+	return c
+print(cout(p.prices,l))
